5474740256af0460e44129325ee70389c33c7afd
foxstudent106239 <mykhailo.kornukh@gmail.com>
2023-12-16 23:13:05 +0200

    add CharCounter obj to field

--- collections1-4/src/mainClasses/LoaderUniqueCharacters.java
+++ collections1-4/src/mainClasses/LoaderUniqueCharacters.java
@@ -1,15 +1,19 @@
 package mainClasses;
 
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class LoaderUniqueCharacters {
     public Map<String, Map<Character, Integer>> cache = new LinkedHashMap<>();
+    CharCounter charCounter = new CharCounter();
 
+    public LoaderUniqueCharacters(){
+
+    }
 
     public Map<Character, Integer> loadingUniqueCharacters(String text) {
         Map<Character, Integer> calculatedUniqueCharactersMap = new LinkedHashMap<>();
-        CharCounter charCounter = new CharCounter();
         if(cache.containsKey(text)){
             calculatedUniqueCharactersMap = cache.get(text);
         }
